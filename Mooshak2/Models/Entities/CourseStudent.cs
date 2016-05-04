0138d74aaf0410e38fb17deeb6564787bc018c5c
indridik <indridi09@ru.is>
2016-05-04 11:35:30 +0000

    Meira úr gamla

---
+++ Mooshak2/Models/Entities/CourseStudent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.Entities
+{
+    /// <summary>
+    /// Setja nemendur í ákveðnum námskeiðum í database'ið
+    /// </summary>
+    public class CourseStudent
+    {
+        /// <summary>
+        /// ID'ið fyrir notandann
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Notandanafn notanda
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// ID'ið fyrir námskeiðið sem nemandinn er skráður í.
+        /// </summary>
+        public int CourseID { get; set; }
+    }
+}
---
+++ Mooshak2/Models/Entities/CourseTeacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.Entities
+{
+    /// <summary>
+    /// Setja nemendur í ákveðnum námskeiðum í database'ið
+    /// </summary>
+    public class CourseTeacher
+    {
+        /// <summary>
+        /// ID'ið fyrir kennarann
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Username kennara
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// ID'ið fyrir námskeiðið sem kennarinn er skráður á.
+        /// </summary>
+        public int CourseID { get; set; }
+
+    }
+}
