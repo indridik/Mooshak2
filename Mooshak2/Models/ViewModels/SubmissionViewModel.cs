d841d7d9d002d428b8f29281a543e03a8cbbb220
Friðrik Hjálmarsson <fridrik14@ru.is>
2016-05-07 14:00:13 +0000

    Stöff

---
+++ Mooshak2/Models/ViewModels/SubmissionViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak2.Models.ViewModels
+{
+    public class SubmissionViewModel
+    {
+        /// <summary>
+        /// Notandanafn nemanda sem skilaði
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Hluti verkefnis sem var skilað
+        /// </summary>
+        public char Milestone { get; set; }
+        /// <summary>
+        /// Niðurstaða skila
+        /// </summary>
+        public string Result { get; set; }
+        /// <summary>
+        /// Forritunartungumál skila
+        /// </summary>
+        public string Language { get; set; }
+        /// <summary>
+        /// Tímasetning skila
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// Hvaða kúrs er verið að skila verkefni í
+        /// </summary>
+        public string Course { get; set; }
+
+    }
+}
--- Mooshak2/Services/SubmissionsService.cs
+++ Mooshak2/Services/SubmissionsService.cs
@@ -9,5 +9,39 @@ namespace Mooshak2.Services
 {
     public class SubmissionsService
     {
+        private ApplicationDbContext _db;
+        public SubmissionsService()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        public SubmissionViewModel GetStudentByID(int studentID)
+        {
+            return null;
+        }
+
+        public SubmissionViewModel GetMilestone(int assignmentID, string MilestoneTitle)
+        {
+            return null;
+        }
+
+        public SubmissionViewModel GetResult(string MilestoneResult)
+        {
+            return null;
+        }
+        public SubmissionViewModel GetLanguage(string AssignmentLanguage)
+        {
+            return null;
+        }
+
+        public SubmissionViewModel GetTime(DateTime subTime)
+        {
+            return null;
+        }
+        public SubmissionViewModel GetCourseByID(int CourseID)
+        {
+            return null;
+        }
+
     }
 }
