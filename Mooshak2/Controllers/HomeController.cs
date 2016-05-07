44f69367a0629750a1edeed083a232dba3c5a0a8
Friðrik Hjálmarsson <fridrik14@ru.is>
2016-05-07 13:40:07 +0000

    lel

--- Mooshak2/Controllers/HomeController.cs
+++ Mooshak2/Controllers/HomeController.cs
@@ -7,22 +7,6 @@ namespace Mooshak2.Controllers
     {
         public ActionResult Index()
         {
-            IdentityManager manager = new IdentityManager();
-            if (!manager.UserExists("admin@ru.is"))
-            {
-                ApplicationUser newAdmin = new ApplicationUser();
-                newAdmin.UserName = "admin@ru.is";
-                manager.CreateUser(newAdmin, "123456");
-                manager.AddUserToRole(newAdmin.Id, "Administrators");
-            }
-            if (!manager.RoleExists("Administrators"))
-            {
-                manager.CreateRole("Administrators");
-            }
-            if (!manager.RoleExists("Teachers"))
-            {
-                manager.CreateRole("Teachers");
-            }
             return View();
         }
 
