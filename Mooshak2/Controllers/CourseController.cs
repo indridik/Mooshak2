ec7e9b1ea67580c69ded4b1ec8fd36af1814c183
Friðrik Hjálmarsson <fridrik14@ru.is>
2016-05-08 16:30:58 +0000

    lel

--- Mooshak2/Controllers/CourseController.cs
+++ Mooshak2/Controllers/CourseController.cs
@@ -23,24 +23,28 @@ namespace Mooshak2.Controllers
         public async Task<ActionResult> CreateCourse(CourseViewModel model)
         {
             ApplicationDbContext _db = new ApplicationDbContext();
-            if (ModelState.IsValid)
+            var exists = _db.Courses.SingleOrDefault(x => x.Name == model.Name);
+            if(exists == null)
             {
                 var course = new Course { ID = model.ID, Name = model.Name };
                 _db.Courses.Add(course);
                 _db.SaveChanges();
-            }
-            var check = _db.Courses.SingleOrDefault(x => x.Name == model.Name);
-            if(check == null)
-            {
-                ///Throw error, (returna í View með modeli í)
-                return View(model);
 
+                var check = _db.Courses.SingleOrDefault(x => x.Name == model.Name);
+                if (check == null)
+                {
+                    ///Throw error, (returna í View með modeli í)
+                    ViewBag.result = "Failed creating course";
+
+                }
+                else
+                {
+                    ViewBag.result = "Course successfully created!";
+                    return View();
+                }
             }
-            else
-            {
-                ViewBag.result = "Course successfully created!";
-                return View();
-            }
+            return View(model);
+
         }
     }
 }
