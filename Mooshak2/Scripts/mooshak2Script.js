f84a101b3984b7e92f97e05a31c932f5bcb86a13
HJALMAR <hjelmut@gmail.com>
2016-05-09 13:58:54 +0000

    jquery/ajax virkar i Index

---
+++ Mooshak2/Scripts/mooshak2Script.js
@@ -0,0 +1,21 @@
+﻿
+
+$(document).ready(function () {
+    $("#courses").change(function () {
+        var id = $(this).children(":selected").attr('id');
+        var form = $("#courseForm");
+        $.ajax({
+            url: "/Home/AssignmentJson/" + id,
+            method: 'POST',
+            data : form.serialize(),
+            success: function(data) {
+                var html = "<select multiple class='form-control' id='assignments'>";
+                for(var i = 0; i < data.length; i++) {
+                    html += "<option>" + data[i].Title + "</option>";
+                }
+                html += "</select";
+                $(".assignmentDiv").html(html);
+;            }
+        });
+    });
+});
--- Mooshak2/Scripts/script.js
+++
@@ -1,8 +0,0 @@
-﻿
-
-//synir verkefni thegar valid er namskeid
-$(document).ready(function () {
-    $('#courses').change(function () {
-        $('#assignments').css('visibility', 'visible');
-    });
-});
