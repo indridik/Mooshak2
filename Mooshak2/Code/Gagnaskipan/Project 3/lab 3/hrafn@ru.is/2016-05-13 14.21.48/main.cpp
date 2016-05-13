f7183dd4be408c693c04581ee3e9e7d99e1054b3
HJALMAR <hjelmut@gmail.com>
2016-05-13 14:41:18 +0000

    fullt af sjitti

---
+++ Mooshak2/Code/Gagnaskipan/Project 3/lab 3/hrafn@ru.is/2016-05-13 14.21.48/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n1, n2,n3;
+	cout << "enter three numbers." << endl;
+	cout << "number1:" << endl;
+	cin >> n1;
+	cout << "number2:" << endl;
+	cin >> n2;
+	cout << "number3:" << endl;
+	cin >> n3;
+	cout << n1 << " + " << n3 << " = " << n1 + n2+ n3;
+}
